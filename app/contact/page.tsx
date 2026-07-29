c898c0b59ccfd36ff8da302a0777ea2aaa1906fa
Gabriel <gabibola955@gmail.com>
2026-07-29 19:42:33 +0100

    Added title metadata for contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,10 @@
 import ContactForm from "@/components/contact/ContactForm";
 import ContactIntro from "../../components/contact/ContactIntro";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Contact | Bolarinwa Gabriel",
+};
 export default function page() {
   return (
     <section className="mt-12 flex flex-wrap gap-6 flex-row">
