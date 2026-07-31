680b3a3f97111f6b8dfdc370faa744304e921b7d
Gabriel <gabibola955@gmail.com>
2026-07-31 19:42:47 +0100

    Responsiveness improvements for case study and minor projects section

--- components/projects/CaseStudySection.tsx
+++ components/projects/CaseStudySection.tsx
@@ -8,14 +8,14 @@ export default function CaseStudySection() {
   return (
     <section className="mt-12 w-full">
       <ProjectSectionHeading word="CASE STUDIES" bracketWord="CASE-STUDY" />
-      <ul className="mt-8 w-full flex items-center flex-row flex-wrap  gap-4">
+      <ul className="mt-8 w-full flex max-lg:justify-center items-center flex-row flex-wrap  gap-4">
         {projects.caseStudies.map((project, index) => (
           <li
             ref={ref}
             style={{ "--i": `1.${index + 1}` } as React.CSSProperties}
             data-animation="hoverInBottom"
             key={project.slug}
-            className=" w-5/6  md:w-[48.816%] lg:w-[31.667%] relative hover:-translate-y-1"
+            className=" w-19/20 md:w-9/20 lg:w-31/100 relative hover:-translate-y-1"
           >
             <CaseStudyCard project={project} />
           </li>
--- components/projects/MinorProjectsSection.tsx
+++ components/projects/MinorProjectsSection.tsx
@@ -8,14 +8,14 @@ export default function MinorProjectsSection() {
   return (
     <section className="mt-12 w-full">
       <ProjectSectionHeading word="MINOR PROJECTS" bracketWord="MINOR" />
-      <ul className="mt-8 w-full flex items-center flex-row flex-wrap  gap-4">
+      <ul className="mt-8 w-full flex items-center max-lg:justify-center flex-row flex-wrap  gap-4">
         {projects.minorProjects.map((project, index) => (
           <li
             ref={ref}
             style={{ "--i": `1.${index + 1}` } as React.CSSProperties}
             data-animation="hoverInBottom"
             key={project.title}
-            className=" w-5/6  md:w-[48.816%] lg:w-[31.667%] relative hover:-translate-y-1"
+            className="w-19/20 md:w-9/20 lg:w-31/100 relative hover:-translate-y-1"
           >
             <MinorProjectCard project={project} />
           </li>
