3563d3f81a39974bcf043455eee52812d61e3078
Gabriel <gabibola955@gmail.com>
2026-07-31 22:58:24 +0100

    Added hover effects to contact button

--- components/home/ContactCard.tsx
+++ components/home/ContactCard.tsx
@@ -5,7 +5,7 @@ function ContactCard() {
     <div className="flex flex-col items-center gap-4 justify-center mx-auto bg-card-background home-contact border border-white/20 rounded-lg w-full text-center mt-16 h-auto py-8 overflow-hidden relative">
       <h3 className="text-lg">Have a project in mind?</h3>
       <Link
-        className="bg-(image:--primary-gradient) py-2 px-5 flex gap-2 items-center text-sm font-bold rounded-full w-fit"
+        className="bg-(image:--primary-gradient) py-2 px-5 flex gap-2 items-center text-sm font-bold rounded-full w-fit hover:-translate-y-1 hover:shadow-neon-hover"
         href={"/contact"}
       >
         <ArrowRight /> Let&apos;s Talk
