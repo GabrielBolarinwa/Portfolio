678818092668bdfb26b80882bac4573b16c0a87f
Gabriel <gabibola955@gmail.com>
2026-08-02 16:14:40 +0100

    Modified footer layout and improved responsiveness

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,13 +17,13 @@ import Link from "next/link";
 export function Footer() {
   return (
     <footer className="footer" id="footer" role="contentinfo">
-      <section className="md:flex flex-col gap-2 items-center w-full md:w-3/10 md:text-center">
-        <h3 className="footer-header font-(family-name:--font-display) font-normal">
+      <section className="flex flex-col items-center w-full text-center">
+        <h3 className="footer-header text-lg font-(family-name:--font-display) font-normal">
           Bolarinwa Gabriel
         </h3>
         <div className="contact-info address flex flex-wrap">
-          <p className="max-sm:mb-4 flex gap-2 items-center justify-center">
-            <MapPin size={16} /> Okitipupa, Ondo State, Nigeria
+          <p className="mb-5 flex gap-2 items-center justify-center">
+            <MapPin /> Okitipupa, Ondo State, Nigeria
           </p>
         </div>
         <div className="contact flex flex-wrap">
@@ -60,58 +60,60 @@ export function Footer() {
           </div>
         </div>
       </section>
-      <section className="md:flex flex-col gap-3 items-center w-full md:w-3/10">
-        <h3 className="footer-header">Navigation</h3>
-        <ul>
-          <li className="hover:text-accent-neon mb-5">
-            <Link href={"/#"} className="flex gap-3 items-center">
-              <Home /> Home
-            </Link>
-          </li>
-          <li className="hover:text-accent-neon mb-5">
-            <Link href={"/about"} className="flex gap-3 items-center">
-              <User /> About
-            </Link>
-          </li>
-          <li className="hover:text-accent-neon mb-5">
-            <Link href={"/projects"} className="flex gap-3 items-center">
-              <BriefcaseBusiness /> Projects
-            </Link>
-          </li>
-          <li className="hover:text-accent-neon mb-5">
-            <Link href={"/contact"} className="flex gap-3 items-center">
-              <Mail /> Home
-            </Link>
-          </li>
-        </ul>
-      </section>
-      <section className="md:flex flex-col gap-3 items-center w-full md:w-3/10 md:text-center md:justify-center">
-        <h3 className="footer-header">More</h3>
-        <ul className="md:justify-center md:flex flex-col gap-4 items-center">
-          <li className="hover:text-accent-neon mb-5">
-            <Link href={"/pong.html"} className="flex gap-3 items-center">
-              <Gamepad2 /> Play a Game
-            </Link>
-          </li>
-          <li className="hover:text-accent-neon mb-5">
-            <Link
-              href={"https://github.com/GabrielBolarinwa/Portfolio"}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex gap-3 items-center truncate"
-            >
-              <GitFork /> View page repository
-            </Link>
-          </li>
-        </ul>
-      </section>
-      <div className="w-full text-center">
+      <div className="flex flex-wrap justify-between w-full gap-y-6">
+        <section className="flex flex-col items-center flex-1 flex-wrap">
+          <h3 className="footer-header">Navigation</h3>
+          <ul className="flex flex-col flex-wrap max-h-27 gap-y-5 gap-x-25">
+            <li className="hover:text-accent-neon">
+              <Link href={"/#"} className="flex gap-3 items-center">
+                <Home /> Home
+              </Link>
+            </li>
+            <li className="hover:text-accent-neon">
+              <Link href={"/about"} className="flex gap-3 items-center">
+                <User /> About
+              </Link>
+            </li>
+            <li className="hover:text-accent-neon">
+              <Link href={"/projects"} className="flex gap-3 items-center">
+                <BriefcaseBusiness /> Projects
+              </Link>
+            </li>
+            <li className="hover:text-accent-neon">
+              <Link href={"/contact"} className="flex gap-3 items-center">
+                <Mail /> Home
+              </Link>
+            </li>
+          </ul>
+        </section>
+        <section className="flex flex-col items-center flex-1 flex-wrap">
+          <h3 className="footer-header">More</h3>
+          <ul className="flex flex-col flex-wrap max-h-27 gap-y-5 gap-x-25 items-center">
+            <li className="hover:text-accent-neon">
+              <Link href={"/pong.html"} className="flex gap-3 items-center">
+                <Gamepad2 /> Play a Game
+              </Link>
+            </li>
+            <li className="hover:text-accent-neon">
+              <Link
+                href={"https://github.com/GabrielBolarinwa/Portfolio"}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex gap-3 items-center truncate"
+              >
+                <GitFork /> View page repository
+              </Link>
+            </li>
+          </ul>
+        </section>
+      </div>
+      <p className="w-full text-center">
         Copyright &copy;{" "}
         <time id="time" dateTime={`${new Date().getFullYear()}`}>
           {new Date().getFullYear()}
         </time>{" "}
         Bolarinwa Gabriel. All Rights Reserved
-      </div>
+      </p>
     </footer>
   );
 }
